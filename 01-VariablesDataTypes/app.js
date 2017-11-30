0f190408dc69388a104c062bec602df9e5d5328f
Naveen Mannam <naveen.mannam@hotmail.com>
2017-11-30 00:14:29 -0500

    Added var const values in variables and datatypes folder

--- 01-VariablesDataTypes/app.js
+++ 01-VariablesDataTypes/app.js
@@ -1,1 +1,13 @@
 "use strict";
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Lexical_grammar#Keywords
+var score;
+console.log(score);
+var livesLeft = 3;
+console.log(livesLeft);
+livesLeft = 2;
+console.log(livesLeft);
+const first_name = "John";
+console.log(first_name);
+// first_name = 'Jane';
+
+// value for const can not be reassigned
